4b7725a8e38eecce4b2cd2b91c2607c2d38d4ec8
Ales <codegod313@gmail.com>
2022-03-09 19:32:59 +0300

    Added comments

--- src/main/java/by/cleverdeath/vita/service/iml/EncryptionServiceImpl.java
+++ src/main/java/by/cleverdeath/vita/service/iml/EncryptionServiceImpl.java
@@ -39,25 +39,25 @@ public class EncryptionServiceImpl implements EncryptionService {
 
     @Override
     public String encryptWithKeyPhrase(String message, String keyPhrase) {
-        Map<Integer, Character> positionsWithCharacters = new HashMap<>();
+        Map<Integer, Character> positionsWithCharacters = new HashMap<>(); // мапа, в которой ключ - позиция буквы в влючевом слове, а значение -  сама буква
         for (int i = 0; i < keyPhrase.length(); i++) {
-            positionsWithCharacters.put(i, keyPhrase.charAt(i));
+            positionsWithCharacters.put(i, keyPhrase.charAt(i)); // заполняем мапу
         }
-        List<Integer> sortedIndexes = positionsWithCharacters.entrySet()
+        List<Integer> sortedIndexes = positionsWithCharacters.entrySet()// сортируем мапу по буквам (чтобы в начале были позиции букв, которые в алфавите стоят раньше)
                 .stream()
                 .sorted(Map.Entry.comparingByValue())
                 .map((Map.Entry::getKey))
                 .toList();
-        String spaces = SPACE.repeat(message.length() % keyPhrase.length());
+        String spaces = SPACE.repeat(message.length() % keyPhrase.length()); // добавляем пробелы, чтобы размер подходил под ключевую фразу
         StringBuffer readyMessageBuffer = new StringBuffer(message);
-        readyMessageBuffer.append(spaces);
+        readyMessageBuffer.append(spaces); // добавляем пробелы
         String readyMessage = readyMessageBuffer.toString();
-        StringBuffer encryptedSequence = new StringBuffer();
-        for (int i = 0; i < readyMessage.length() / keyPhrase.length(); i++) {
-            int startSubarrayIndex = i * keyPhrase.length();
-            sortedIndexes.forEach(x -> {
-                int currentCharIndex = startSubarrayIndex + x;
-                encryptedSequence.append(readyMessage.charAt(currentCharIndex));
+        StringBuffer encryptedSequence = new StringBuffer(); // зашифрованное сообщение
+        for (int i = 0; i < readyMessage.length() / keyPhrase.length(); i++) { // цикл по блокам, размеров с ключевую фразу
+            int startSubarrayIndex = i * keyPhrase.length(); // вычисляем индекс начала блока
+            sortedIndexes.forEach(x -> { // цикл по всем элементам
+                int currentCharIndex = startSubarrayIndex + x; // начальный индекс + индекс из наших отсортированных (собираем шифр по порядку вхождения букв улючевой фразы в алфавит)
+                encryptedSequence.append(readyMessage.charAt(currentCharIndex)); // добавляем букву в выходную последовательность
             });
         }
         return encryptedSequence.toString();
@@ -65,11 +65,11 @@ public class EncryptionServiceImpl implements EncryptionService {
 
     @Override
     public String encryptWithGrid(String message, Integer gridDimension, List<GridPosition> positions) {
-        Character[][] grid = new Character[gridDimension][gridDimension];
-        String spaces = SPACE.repeat(message.length() % gridDimension);
-        StringBuffer readyMessageBuffer = new StringBuffer(message);
-        readyMessageBuffer.append(spaces);
-        String readyMessage = readyMessageBuffer.toString();
+        Character[][] grid = new Character[gridDimension][gridDimension]; // создание решётки, ращмерностью, переданной в параметры метода
+        String spaces = SPACE.repeat(message.length() % gridDimension);// дополняем шифруемый текст польностью, чтобы решётка была полностью заполненной
+        StringBuffer readyMessageBuffer = new StringBuffer(message); // зашифрованное сообщение (передаём в конструктор сообщение)
+        readyMessageBuffer.append(spaces); //добавляем пробелы
+        String readyMessage = readyMessageBuffer.toString(); // готовое сообщение
         for (int i = 0; i < readyMessage.length() / gridDimension; i++) {
             int currentCharacter = i * gridDimension;
             for (int j = 0; j < positions.size(); j++) {
@@ -88,7 +88,7 @@ public class EncryptionServiceImpl implements EncryptionService {
     }
 
     @Override
-    public String encryptCesar(String message, Integer encryptionKey) {
+    public String encryptCesar(String message, Integer encryptionKey) { // тут всё просто по формулам
         StringBuffer encryptedSequence = new StringBuffer(message);
         for (int i = 0; i < message.length(); i++) {
             Character currentChar = message.charAt(i);
@@ -108,15 +108,15 @@ public class EncryptionServiceImpl implements EncryptionService {
     private Character[][] rotateGrid(Character[][] grid) {
         Character[][] rotatedGrid = new Character[grid.length][grid.length];
         for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid.length; j++) {
-                rotatedGrid[j][grid.length - i - 1] = grid[i][j];
+            for (int j = 0; j < grid.length; j++) { // проходим по решётке
+                rotatedGrid[j][grid.length - i - 1] = grid[i][j]; // значение из обычной решётки записываем в ячейку в повёрнутой решётке с противоположными координатами
             }
         }
         return rotatedGrid;
     }
 
     @Override
-    public String encryptWithSubstitution(String message, Integer encryptionKey) {
+    public String encryptWithSubstitution(String message, Integer encryptionKey) { // тут тоже всё по формулам
         StringBuffer encryptedSequence = new StringBuffer(message);
         for (int i = 0; i < message.length(); i++) {
             Character currentChar = message.charAt(i);
