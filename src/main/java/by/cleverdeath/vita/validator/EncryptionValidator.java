d7084147f205c59fa8c801e80f329100244f5957
Ales <codegod313@gmail.com>
2022-03-09 20:33:17 +0300

    Added EncryptionValidator

---
+++ src/main/java/by/cleverdeath/vita/validator/EncryptionValidator.java
@@ -0,0 +1,16 @@
+package by.cleverdeath.vita.validator;
+
+import by.cleverdeath.vita.entity.GridPosition;
+
+import java.util.List;
+
+public interface EncryptionValidator {
+    boolean validateHedgeParameters(String message, Integer height);
+
+    boolean validateKeyPhraseParameters(String message, String keyPhrase);
+
+    boolean validateGridParameters(String message, Integer gridDimension, List<GridPosition> positions);
+
+
+    boolean validateSubstitutionTypeParameters(String message, Integer encryptionKey);
+}
