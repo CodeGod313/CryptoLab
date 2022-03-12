72d0684c89010f8aa693892a1c90140f93a8c6ec
Ales <codegod313@gmail.com>
2022-03-12 11:46:55 +0300

    Added logs for CrossTest

--- src/test/java/by/cleverdeath/vita/service/iml/CrossTest.java
+++ src/test/java/by/cleverdeath/vita/service/iml/CrossTest.java
@@ -3,6 +3,8 @@ package by.cleverdeath.vita.service.iml;
 import by.cleverdeath.vita.entity.GridPosition;
 import by.cleverdeath.vita.service.DecryptionService;
 import by.cleverdeath.vita.service.EncryptionService;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,7 @@ import java.util.List;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CrossTest {
+    static Logger logger = LogManager.getLogger(CrossTest.class);
     EncryptionService encryptionService;
     DecryptionService decryptionService;
     String expected;
@@ -21,12 +24,16 @@ public class CrossTest {
         encryptionService = new EncryptionServiceImpl();
         decryptionService = new DecryptionServiceImpl();
         expected = "aboba"; // :*
+        logger.info("Message: " + expected);
     }
 
     @Test
     void testHedge() {
         String encrypted = encryptionService.encryptWithHedge(expected, 4).get();
         String actual = decryptionService.decryptWithHedge(encrypted, 4).get().trim();
+        logger.info("Algorithm: hedge");
+        logger.info("Encrypted message: " + encrypted);
+        logger.info("Decrypted message: " + actual);
         Assertions.assertEquals(expected, actual);
     }
 
@@ -34,6 +41,9 @@ public class CrossTest {
     void testKeyPhrase() {
         String encrypted = encryptionService.encryptWithKeyPhrase(expected, "KEYPHRASE").get();
         String actual = decryptionService.decryptWithKeyPhrase(encrypted, "KEYPHRASE").get().trim();
+        logger.info("Algorithm: key phrase");
+        logger.info("Encrypted message: " + encrypted);
+        logger.info("Decrypted message: " + actual);
         Assertions.assertEquals(expected, actual);
     }
 
@@ -47,6 +57,9 @@ public class CrossTest {
                 new GridPosition(2, 2));
         String encrypted = encryptionService.encryptWithGrid(expected, gridDimension, positions).get();
         String actual = decryptionService.decryptWithGrid(encrypted, gridDimension, positions).get().trim();
+        logger.info("Algorithm: grid");
+        logger.info("Encrypted message: " + encrypted);
+        logger.info("Decrypted message: " + actual);
         Assertions.assertEquals(expected, actual);
     }
 
@@ -54,6 +67,9 @@ public class CrossTest {
     void testCesar() {
         String encrypted = encryptionService.encryptCesar(expected, 4).get();
         String actual = decryptionService.decryptCesar(encrypted, 4).get().trim();
+        logger.info("Algorithm: Cesar");
+        logger.info("Encrypted message: " + encrypted);
+        logger.info("Decrypted message: " + actual);
         Assertions.assertEquals(expected, actual);
     }
 
@@ -61,6 +77,9 @@ public class CrossTest {
     void testSubstitution() {
         String encrypted = encryptionService.encryptWithSubstitution(expected, 7).get();
         String actual = decryptionService.decryptWithSubstitution(encrypted, 15).get().trim();
+        logger.info("Algorithm: Substitution");
+        logger.info("Encrypted message: " + encrypted);
+        logger.info("Decrypted message: " + actual);
         Assertions.assertEquals(expected, actual);
     }
 }
